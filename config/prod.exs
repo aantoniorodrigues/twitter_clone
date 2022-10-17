37a06bb920c044a2c9cd94608c0bbe827f6fb941
aantoniorodrigues <antoniocvpr@gmail.com>
2022-10-17 13:36:26 +0200

    details for deployment

--- config/prod.exs
+++ config/prod.exs
@@ -11,7 +11,8 @@ import Config
 # before starting your production server.
 config :chirp, ChirpWeb.Endpoint,
   cache_static_manifest: "priv/static/cache_manifest.json",
-  force_ssl: [rewrite_on: [:x_forwarded_proto]]
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  check_origin: ["https://clone.gigalixirapp.com"]
 
 config :chirp, ChirpWeb.Repo,
   ssl: true
